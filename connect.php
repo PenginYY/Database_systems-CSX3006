b71f6dea226f138a2ea7aafee31f497ef309e045
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-02-28 22:25:13 +0700

    add form tag

--- connect.php
+++ connect.php
@@ -4,13 +4,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
   // Create connection
   $conn = mysqli_connect('localhost', 'root', '', 'Hotel') or die('Connection failed: ' . $conn->connect_error);
 
-  $duplicate=mysqli_query($conn,"SELECT Email FROM `Account` WHERE email='$email'");
-  if (mysqli_num_rows($duplicate)>0)
-  {
-  $exist_email = "Email has already exist";
-  header("Location: http://localhost:3000/c_signup.php?:");
-  echo $exist_email;
-  } 
   
   if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['name']) && isset($_POST['surname'])) {
     $email = $_POST['email'];
@@ -18,8 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
     $name = $_POST['name'];
     $surname = $_POST['surname'];
     $address = $_POST['address'];
-    echo "Hello";
-   
+
     $sql = "INSERT INTO `Account` (`Email`, `Password`, `Name`, `Surname`, `Address`) VALUES ('$email', '$userPassword', '$name', '$surname', '$address')";
 
     $result = mysqli_query($conn, $sql);
